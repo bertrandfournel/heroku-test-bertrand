649260fa0b1c14ccdca6afa4a7f92bc8e263c50c
Bertrand Fournel <fournel.bertrand@gmail.com>
2024-08-23 11:11:20 +0200

    first

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Mon Application Streamlit Simple")
+
+st.subheader("Bienvenue sur cette application !")
+
+st.write("Ceci est une application Streamlit très simple.")
+
+if st.button("Cliquez ici"):
+    st.write("Bienvenue sur cette application Streamlit !")
